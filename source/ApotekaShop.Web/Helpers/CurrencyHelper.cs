1580b697702798bc13222e3c1cac39fe85b686a0
Aliaksandr_Bialinski <tremi1990@gmail.com>
2016-06-20 12:45:54 +0300

    Add currency helper

---
+++ source/ApotekaShop.Web/Helpers/CurrencyHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ApotekaShop.Web.Helpers
+{
+    public static class CurrencyHelper
+    {
+        public static string Currency(this HtmlHelper helper, int? price)
+        {
+            if (!price.HasValue) return string.Empty;
+
+            return String.Format("DKK {0}", price.Value.ToString("N2"));
+        }
+    }
+}
