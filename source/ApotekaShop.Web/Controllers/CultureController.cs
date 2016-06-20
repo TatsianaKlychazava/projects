8693096be5360c2630392d7d682078907852e232
Aliaksandr_Bialinski <tremi1990@gmail.com>
2016-06-20 17:18:44 +0300

    Add cullture dropdown draft

---
+++ source/ApotekaShop.Web/Controllers/CultureController.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Web.Mvc;
+using ApotekaShop.Web.Models;
+
+namespace ApotekaShop.Web.Controllers
+{
+    public class CultureController : Controller
+    {
+        // GET: Culture
+        public ActionResult Index()
+        {
+            return PartialView(new CultureViewModel() {SelectedCulture = CultureInfo.CurrentCulture});
+        }
+
+        public RedirectResult SetCulture(string culture)
+        { 
+
+            CultureInfo.CurrentCulture = new CultureInfo(culture);
+            return Redirect(Request.UrlReferrer.AbsoluteUri);
+        }
+    }
+}
---
+++ source/ApotekaShop.Web/Models/CultureViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+
+namespace ApotekaShop.Web.Models
+{
+    public class CultureViewModel
+    {
+        public IEnumerable<CultureInfo> Cultures => CultureInfo.GetCultures(CultureTypes.AllCultures)
+            .Except(CultureInfo.GetCultures(CultureTypes.SpecificCultures));
+        
+        public CultureInfo SelectedCulture { get; set; }
+    }
+}
