8c462bdc72671bd1cc9afea02dcb7675f55162d1
Dzianis Shchasnovich <dzianis@gmail.com>
2016-06-27 16:10:50 +0300

    added sitemap

--- source/ApotekaShop.Web/WebContext.cs
+++ source/ApotekaShop.Web/WebContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Web;
 using ApotekaShop.Services.Helpers;
@@ -16,6 +17,48 @@ namespace ApotekaShop.Web
         public WebContext(HttpContextBase context)
         {
             _context = context;
+        }
+
+        private bool? _isConnectionSecured;
+        public bool IsConnectionSecured
+        {
+            get
+            {
+                if (_isConnectionSecured == null)
+                {
+                    _isConnectionSecured = false;
+                    if (_context != null && _context.Request != null)
+                    {
+                        _isConnectionSecured = _context.Request.IsSecureConnection;
+                        //Or maybe SSL offloaded on reverse-proxy
+                        _isConnectionSecured |= _context.Request.Params.AllKeys.Any(x => string.Equals(x, "HTTP_X_ARR_SSL", StringComparison.InvariantCultureIgnoreCase));
+                    }
+                }
+
+                return _isConnectionSecured.Value;
+            }
+        }
+
+        private string _protocol = null;
+        public string Protocol
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_protocol))
+                    _protocol = IsConnectionSecured ? "https" : "http";
+                return _protocol;
+            }
+        }
+
+        private string _httpHost = null;
+        public string HttpHost
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_httpHost))
+                    _httpHost = GetServerVariables("HTTP_HOST");
+                return _httpHost;
+            }
         }
 
         public void SetCountry(Country country)
@@ -40,6 +83,31 @@ namespace ApotekaShop.Web
             }
 
             return country;
+        }
+
+        private string GetServerVariables(string name)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                if (_context?.Request == null)
+                    return result;
+
+                //should be in try-catch 
+                //as described here http://www.nopcommerce.com/boards/t/21356/multi-store-roadmap-lets-discuss-update-done.aspx?p=6#90196
+                if (_context.Request.ServerVariables[name] != null)
+                {
+                    result = _context.Request.ServerVariables[name];
+                }
+                if (string.IsNullOrWhiteSpace(result))
+                    result = _context.Request.Url.DnsSafeHost;
+            }
+            catch
+            {
+                result = string.Empty;
+            }
+            return result;
         }
     }
 }
