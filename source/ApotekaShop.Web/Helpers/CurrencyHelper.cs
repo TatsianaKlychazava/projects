d7a761339e5539f1c8b0792da88071ad8528a37e
Alexey Novik <alexeynovik@leaptest.com>
2016-07-07 13:28:22 +0300

    Modifying product cards.

--- source/ApotekaShop.Web/Helpers/CurrencyHelper.cs
+++ source/ApotekaShop.Web/Helpers/CurrencyHelper.cs
@@ -9,7 +9,7 @@ namespace ApotekaShop.Web.Helpers
     {
         public static string Currency(this HtmlHelper helper, int? price, bool onlyPrice = false)
         {
-            if (!price.HasValue) return string.Empty;
+            if (!price.HasValue) return "N/A";
             double currentPrice = price.Value / 100.00;
             return onlyPrice ? currentPrice.ToString("F2", CultureInfo.InvariantCulture) :
                 $"{ShopResources.CurrencyName} {currentPrice.ToString("F2", CultureInfo.InvariantCulture)}";
